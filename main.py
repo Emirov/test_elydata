c744dc430d8835d8aa4817f3a96aa9a1ac099d64
Emirov <emirmaaoui@yahoo.fr>
2022-08-31 11:01:55 +0100

    feat: add rendering in `scrape_load_db`

--- main.py
+++ main.py
@@ -20,5 +20,5 @@ def scrape_load_db(name: str):
     MONGO_USER=os.getenv('MONGO_USER')
     MONGO_PASS=os.getenv('MONGO_PASS')
     uri = f'mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:{MONGO_PORT}'
-
     result = fbs.load_data(uri, 'admin', 'fb_page_data',  json_data)
+    return result
