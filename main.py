92b6b89045d2994598af94decb5219cc170611f4
Emirov <emirmaaoui@yahoo.fr>
2022-08-31 11:00:54 +0100

    feat: updte `scrape_load_db` function

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
-import scraper
+from scraper import *
+import os
+from dotenv import load_dotenv
+load_dotenv()
 app = FastAPI()
 
-
-@app.get("/")
 def home_root():
     '''home root'''
     return {"Hello": "World"}
+
+
+@app.get("/page/{name}")
+def scrape_load_db(name: str):
+    '''root to scrape fb page and load to db'''
+    fbs = fb_scraper
+    json_data  =  fbs.scrape_data(name)
+    MONGO_HOST=os.getenv('MONGO_HOST')
+    MONGO_PORT=os.getenv('MONGO_PORT')
+    MONGO_USER=os.getenv('MONGO_USER')
+    MONGO_PASS=os.getenv('MONGO_PASS')
+    uri = f'mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:{MONGO_PORT}'
+
+    result = fbs.load_data(uri, 'admin', 'fb_page_data',  json_data)
